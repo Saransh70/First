3d2de1990001aa298fbfcf641e17f116eed9a10d
Saransh Kharel <saranshromash@gmail.com>
2021-03-02 09:59:58 +0545

    hour calculation

--- LabExercise/four.py
+++ LabExercise/four.py
@@ -1,1 +1,8 @@
-a = 10
+n =int(input("enter the minutes passed since midnight"))
+hours=(n//60)
+minutes=(n%60)
+print(f"the hours is {hours}")
+
+print(f"the minutes is {minutes}")
+print(f"the minutes is {hours}: {minutes} now")
+#this is a try and lab exercise
