e19fa61ab4a29b07cec619790dbc7341f10b1d4c
Saransh Kharel <saranshromash@gmail.com>
2021-03-10 23:05:28 +0545

    covert given second to day, hour and minutes

---
+++ LabExercise/nine.py
@@ -0,0 +1,4 @@
+#write a python program to find the sum of the first n positive integers sum=(n*(n+1))/2
+n=int(input("enter your positive interger here:"))
+sum= (n*(n+1))/2
+print(f"your sum of first number is {sum}")
---
+++ LabExercise/ten.py
@@ -0,0 +1,11 @@
+#write a python program to convert seconds to day ,hour ,minutes and seconds
+a=int(input("enter how much second would you like to convert to day ,hour ,minutes and seconds :"))
+day=a/(60*60*24)
+hour=a/(60*60)
+minutes=a/60
+print(f"Your converted hour time is :{hour}")
+print(f"Your converted day time is :{day}")
+print(f"Your converted minute time is:{minutes}")
+print(f"Your given second is:{a}")
+
+
