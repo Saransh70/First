c7b557ba9addaeecc5b824fab17975b05e54c42b
Saransh Kharel <saranshromash@gmail.com>
2021-03-23 09:21:23 +0545

    first test

--- LabExercise/ten.py
+++ LabExercise/ten.py
@@ -1,4 +1,5 @@
 #write a python program to convert seconds to day ,hour ,minutes and seconds
+#changes
 a=int(input("enter how much second would you like to convert to day ,hour ,minutes and seconds :"))
 day=a/(60*60*24)
 hour=a/(60*60)
